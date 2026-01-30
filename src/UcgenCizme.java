f7a03991e556c97566d114fab23a4a1ca59edbd5
Eren <muhammeterenmente2@gmail.com>
2026-01-30 17:32:23 +0300

    Ucgen çizme algoritması yapıldı.

--- src/UcgenCizme.java
+++ src/UcgenCizme.java
@@ -12,18 +12,19 @@ public class UcgenCizme {
     }
 
     static void cizdir(int yukseklik) {
-        int sayac = 1 ;
 
-        for(int i = 0;i < yukseklik;i++){
-            for(int j = 0; j < yukseklik - i; j++ ) {System.out.print(" ");}
+        for (int i = 0; i < yukseklik; i++) {
+            for (int j = 0; j < yukseklik - i - 1; j++) {
+                System.out.print(" ");
+            }
 
-            for(int k = 0; k < sayac + i; k++){System.out.print("*");}
+            for (int k = 0; k < 2 * i + 1; k++) {
+                System.out.print("*");
+            }
 
-            sayac++;
             System.out.println();
 
         }
-        
 
     }
 
